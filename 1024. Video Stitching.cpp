ffbd451be6e0591c5e8873ec2803ca8a5b79fdef
Miao Wang <wangnk2012@gmail.com>
2019-04-24 13:02:10 -0400

    Update 1024. Video Stitching.cpp

--- 1024. Video Stitching.cpp
+++ 1024. Video Stitching.cpp
@@ -1,7 +1,10 @@
 /*
-You are given a series of video clips from a sporting event that lasted T seconds.  These video clips can be overlapping with each other and have varied lengths.
-Each video clip clips[i] is an interval: it starts at time clips[i][0] and ends at time clips[i][1].  We can cut these clips into segments freely: for example, a clip [0, 7] can be cut into segments [0, 1] + [1, 3] + [3, 7].
-Return the minimum number of clips needed so that we can cut the clips into segments that cover the entire sporting event ([0, T]).  If the task is impossible, return -1.
+You are given a series of video clips from a sporting event that lasted T seconds.  These video clips can be overlapping with
+each other and have varied lengths.
+Each video clip clips[i] is an interval: it starts at time clips[i][0] and ends at time clips[i][1].  We can cut these clips 
+into segments freely: for example, a clip [0, 7] can be cut into segments [0, 1] + [1, 3] + [3, 7].
+Return the minimum number of clips needed so that we can cut the clips into segments that cover the entire sporting event ([0, T]).  
+If the task is impossible, return -1.
 
 Example 1:
 	Input: clips = [[0,2],[4,6],[8,10],[1,9],[1,5],[5,9]], T = 10
