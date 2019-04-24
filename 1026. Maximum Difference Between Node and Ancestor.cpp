c941c88b0db7f0842df3f7bf2452cca2b599bf24
Miao Wang <wangnk2012@gmail.com>
2019-04-24 12:57:08 -0400

    1026. Maximum Difference Between Node and Ancestor

---
+++ 1026. Maximum Difference Between Node and Ancestor.cpp
@@ -0,0 +1,54 @@
+/*
+Given the root of a binary tree, find the maximum value V for which there exists different nodes A and B where V = |A.val - B.val| and A is an ancestor of B.
+(A node A is an ancestor of B if either: any child of A is equal to B, or any child of A is an ancestor of B.)
+
+Example 1:
+	Input: [8,3,10,1,6,null,14,null,null,4,7,13]
+	Output: 7
+	Explanation:
+	We have various ancestor-node differences, some of which are given below :
+	|8 - 3| = 5
+	|3 - 7| = 4
+	|8 - 1| = 7
+	|10 - 13| = 3
+	Among all possible differences, the maximum value of 7 is obtained by |8 - 1| = 7.
+
+Note:
+	The number of nodes in the tree is between 2 and 5000.
+	Each node will have value between 0 and 100000.
+*/
+
+//Definition for a binary tree node.
+ struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+
+class Solution {
+public:
+	int maxAncestorDiff(TreeNode* root) {
+		int res = 0;
+		if (!root) return 0;
+		stack<TreeNode*> temp;
+		temp.push(root);
+		while (!temp.empty()) {
+			auto a = temp.top();
+			temp.pop();
+			if (a->left) temp.push(a->left);
+			if (a->right) temp.push(a->right);
+			helper(a, a->val, res);
+		}
+		return res;
+	}
+
+	void helper(TreeNode* node, int ori, int& res) {
+		if (!node) return;
+		res = max(res, abs(node->val - ori));
+		helper(node->left, ori, res);
+		helper(node->right, ori, res);
+		return;
+	}
+};
