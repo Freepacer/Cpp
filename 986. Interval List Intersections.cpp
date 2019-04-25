f7d57d02c1b237fee367f001f74f6e0ef3268f26
Miao Wang <wangnk2012@gmail.com>
2019-04-25 14:46:45 -0400

    986. Interval List Intersections

---
+++ 986. Interval List Intersections.cpp
@@ -0,0 +1,86 @@
+/*
+Given two lists of closed intervals, each list of intervals is pairwise disjoint and in sorted order.
+Return the intersection of these two interval lists.
+(Formally, a closed interval [a, b] (with a <= b) denotes the set of real numbers x with a <= x <= b.  
+The intersection of two closed intervals is a set of real numbers that is either empty, or can be represented as a closed interval.  
+For example, the intersection of [1, 3] and [2, 4] is [2, 3].)
+
+Example 1:
+	Input: A = [[0,2],[5,10],[13,23],[24,25]], B = [[1,5],[8,12],[15,24],[25,26]]
+	Output: [[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]]
+	Reminder: The inputs and the desired output are lists of Interval objects, and not arrays or lists.
+
+Note:
+	0 <= A.length < 1000
+	0 <= B.length < 1000
+	0 <= A[i].start, A[i].end, B[i].start, B[i].end < 10^9
+*/
+
+
+//Definition for an interval.
+struct Interval {
+	int start;
+	int end;
+	Interval() : start(0), end(0) {}
+	Interval(int s, int e) : start(s), end(e) {}
+};
+
+class Solution {
+public:
+	vector<Interval> intervalIntersection(vector<Interval>& A, vector<Interval>& B) {
+		vector<Interval> res;
+		int i = 0;
+		int j = 0;
+		while (i < A.size() && j < B.size()) {
+			int tempstart = 0;
+			int tempend = 0;
+			if (A[i].end >= B[j].start && A[i].start <= B[j].end) {
+				if (B[j].end >= A[i].end) {
+					if (A[i].start <= B[j].start) {
+						tempstart = B[j].start;
+						tempend = A[i].end;
+						Interval temp(tempstart, tempend);
+						res.push_back(temp);
+					}
+					else
+					{
+						tempstart = A[i].start;
+						tempend = A[i].end;
+						Interval temp(tempstart, tempend);
+						res.push_back(temp);
+					}
+				}
+				else
+				{
+					if (A[i].start <= B[j].start) {
+						tempstart = B[j].start;
+						tempend = B[j].end;
+						Interval temp(tempstart, tempend);
+						res.push_back(temp);
+					}
+					else
+					{
+						tempstart = A[i].start;
+						tempend = B[j].end;
+						Interval temp(tempstart, tempend);
+						res.push_back(temp);
+					}
+				}
+			}
+			if (A[i].end == B[j].end)
+			{
+				i++;
+				j++;
+				continue;
+			}
+			else
+			{
+				if (A[i].end < B[j].end)
+					i++;
+				else
+					j++;
+			}
+		}
+		return res;
+	}
+};
